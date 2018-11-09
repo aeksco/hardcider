d07fe58bbdcc48380352041ef8f8b8d0a5bfb831
Alexander Schwartzberg <aeksco@gmail.com>
2018-11-08 23:22:46 -0500

    First commit!

---
+++ README.md
@@ -0,0 +1,26 @@
+# hardcider
+:beer: Create citations for websites quickly from the command line
+
+
+### Installation
+Run the following command to install `refa` - requires Node.js 8.x and higher
+
+```bash
+npm install -g hardcider
+```
+
+### Usage
+Use the `hardcider website` command to create a citation. Example:
+
+```bash
+hardcider website --ieee https://www.merriam-webster.com/dictionary/cat
+```
+
+**Flags**
+
+- `--ieee` - returns an IEEE Formatted Citation
+- `--mla` - returns an MLA Formatted Citation
+
+### Thanks
+
+Built with [commanderjs](https://github.com/tj/commander.js/), [chalk](https://github.com/chalk/chalk), [puppeteer](https://github.com/GoogleChrome/puppeteer/), and [citationmachine.net](http://www.citationmachine.net/). Open Source under the [MIT License](https://opensource.org/licenses/MIT).
---
+++ bin/hardcider.js
@@ -0,0 +1,95 @@
+#!/usr/bin/env node
+const chalk = require('chalk')
+const semver = require('semver')
+const minimist = require('minimist')
+const program = require('commander')
+
+// Search Constants
+const CITATION_URL_IEEE ='http://www.citationmachine.net/ieee/cite-a-website'
+const CITATION_URL_MLA ='http://www.citationmachine.net/mla/cite-a-website'
+
+// // // //
+
+program
+  .version(require('../package').version)
+  .usage('<command> [options]')
+
+program
+  .command('website <website-url>')
+  .description('fetch a basic citation for the website-url argument')
+  .option('--ieee', 'IEEE Format citation')
+  .option('--mla', 'MLA Formatted citation')
+  .action((websiteUrl, cmd) => {
+
+    // Pulls options from command
+    let fetchUrl = ''
+    const options = cleanArgs(cmd)
+
+    if (options.mla) {
+      fetchUrl = CITATION_URL_MLA
+      console.log(`\n${chalk.blue(`Fetching MLA citation...`)}\n`)
+    } else {
+      fetchUrl = CITATION_URL_IEEE
+      console.log(`\n${chalk.blue(`Fetching IEEE citation...`)}\n`)
+    }
+
+    // Logs start prompt
+    require('../lib/fetch')(fetchUrl, websiteUrl)
+    .then((resp) => {
+
+      // Logs citations
+      console.log(chalk.yellow(resp))
+      console.log(`\n`)
+
+      // Roll credits
+      // console.log(`\n${chalk.blue(`Built by @aeksco - Powered by citationmachine.com`)}\n`)
+
+      // Exit the CLI
+      process.exit()
+    })
+
+  })
+
+// output help information on unknown commands
+program
+  .arguments('<command>')
+  .action((cmd) => {
+    program.outputHelp()
+    console.log(`  ` + chalk.red(`Unknown command ${chalk.yellow(cmd)}.`))
+    console.log()
+  })
+
+// add some useful info on help
+program.on('--help', () => {
+  console.log()
+  console.log(`  Run ${chalk.cyan(`hardcider <command> --help`)} for detailed usage of given command.`)
+  console.log()
+})
+
+program.commands.forEach(c => c.on('--help', () => console.log()))
+
+// Parse arguments into commander program
+program.parse(process.argv)
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp()
+}
+
+function camelize (str) {
+  return str.replace(/-(\w)/g, (_, c) => c ? c.toUpperCase() : '')
+}
+
+// commander passes the Command object itself as options,
+// extract only actual options into a fresh object.
+function cleanArgs (cmd) {
+  const args = {}
+  cmd.options.forEach(o => {
+    const key = camelize(o.long.replace(/^--/, ''))
+    // if an option is not present and Command has a method with the same name
+    // it should not be copied
+    if (typeof cmd[key] !== 'function' && typeof cmd[key] !== 'undefined') {
+      args[key] = cmd[key]
+    }
+  })
+  return args
+}
